afe0c77191d6fdf8cc0aaee3f57de926b2d4ef06
Abai Olumide Victor <91828103+DevBrain71@users.noreply.github.com>
2022-09-18 22:01:00 +0100

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "./footer.css"
+
+function Footer() {
+    return (
+        <div className='bg-secondary p-3 d-flex m-0 justify-content-around'>
+          <a href="#"><i class="icon fa-brands fa-square-facebook"></i></a>
+          <a href="#"><i class=" icon fa-brands fa-square-instagram"></i></a>
+          <a href="#"><i class="icon fa-brands fa-square-twitter"></i></a>
+          <a href="#"><i class="icon fa-brands fa-pinterest"></i></a>
+          <a href="#"><i class="icon fa-brands fa-square-github"></i></a>   
+          <a href="#"><p>Privacy policy</p></a>
+          <a href="#"><p>Legal Terms</p></a>
+          <a href="#"><p>Terms & Conditions</p></a>
+          <a href="#"><p>AboutUs</p></a>
+          <a href="#"><p>Contact Us</p></a>
+        </div>
+      )
+}
+export default Footer ;
