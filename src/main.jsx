c51bd35eedf8299505a071e26f24d2a345f38de4
Abai Olumide <devbrain71@gmail.com>
2022-09-26 17:39:39 +0100

    Updated Text-Editor

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {path: '/textedit', element: <Texteditor/>},
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
