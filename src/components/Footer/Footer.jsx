27833d83b9ed8a67feee2cdc2ec292852eac769e
unknown <adegbitedavid01@gmail.com>
2022-10-19 13:03:53 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,158 +1,104 @@
 import "./footer.css"
 
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa"; 
+import { FaLinkedin } from "react-icons/fa"; 
+import { FaInstagram } from "react-icons/fa"; 
+
 function Footer() {
     return (
-        // <div className='bg-secondary p-3 d-flex m-0 justify-content-around'>
-        //   <a href="#"><i class="icon fa-brands fa-square-facebook"></i></a>
-        //   <a href="#"><i class=" icon fa-brands fa-square-instagram"></i></a>
-        //   <a href="#"><i class="icon fa-brands fa-square-twitter"></i></a>
-        //   <a href="#"><i class="icon fa-brands fa-pinterest"></i></a>
-        //   <a href="#"><i class="icon fa-brands fa-square-github"></i></a>   
-        //   <a href="#"><p>Privacy policy</p></a>
-        //   <a href="#"><p>Legal Terms</p></a>
-        //   <a href="#"><p>Terms & Conditions</p></a>
-        //   <a href="#"><p>AboutUs</p></a>
-        //   <a href="#"><p>Contact Us</p></a>
-        // </div>
+        <div className="">
+          <section className="footer-hero">
+            <p>Get connected with us on social networks</p>
 
+            <div>
+              <FaFacebookF />
+              <FaTwitter />
+              <FaLinkedin />
+              <FaInstagram />
+            </div>  
+          </section>
 
+         <section>
+           <div className="footer">
+            <div className="footer text-start">   
+              <div className="row primary">
+                <div className="column blog">
+                  <h3 className="text-info">The Arts Of The Mind</h3>
+        <img src="./mascot-logo.jpg" className="card-img" alt="background" />
 
-        //New footer
+                </div>
 
-<>
-    
-    <div className="footer">
-    <div className="footer text-start">
-                
-                <div class="row primary">
-             <div class="column blog">
-           
-             <h3 className="text-info">BlogApp</h3>
-           
-            
-             <div class="social">
-               <i class="fa-brands fa-facebook-square"></i>
-               <i class="fa-brands fa-instagram-square"></i>
-               <i class="fa-brands fa-twitter-square"></i>
-               <i class="fa-brands fa-youtube-square"></i>
-               <i class="fa-brands fa-whatsapp-square"></i>
-             </div>
-           </div>
-
-           <div class="column links">
-             {/* <h3>Some Links</h3> */}
-              <ul>
-               <li>
-                <a href="#faq">NEWS</a>
-               </li>
-               <li>
-                <a href="#cookies-policy">POLITICS</a>
-               </li>
-               <li>
-               <a href="#terms-of-services">ENTERTAINMENT</a>
-               </li>
-               <li>
-               <a href="#support">SPORT</a>
-               </li>
-             </ul>
-           </div>
-           
-           <div class="column links">
-         {/* <h4>Bad man</h4> */}
-           
+           <div className="column links">
+            <h4>Categories</h4>
             <ul>
-           
-             <li>
-              <a href="#faq">ABOUT US</a>
-             </li>
-             <li>
-              <a href="#cookies-policy">ADVERTISE</a>
-             </li>
-             <li>
-              <a href="#terms-of-services">CONTACT US</a>
-             </li>
-             <li>
-              <a href="#support">RSS</a>
-             </li>
-             <li>
-              <a href="#support"> FAQ <li>
-              
-             </li></a>
-             </li>
+              <li> <a href="#faq">NEWS</a> </li>
+              <li> <a href="#cookies-policy">POLITICS</a> </li>
+              <li> <a href="#terms-of-services">ENTERTAINMENT</a> </li>
+              <li> <a href="#support">SPORT</a> </li>
+              <li> <a href="#support">TECH</a> </li>
             </ul>
-           
            </div>
            
+           {/* <div className="column links">
+              <ul>         
+                <li> <a href="#faq">ABOUT US</a> </li>
+                <li> <a href="#cookies-policy">ADVERTISE</a> </li>
+                <li> <a href="#terms-of-services">CONTACT US</a> </li>
+                <li> <a href="#support">RSS</a> </li>
+                <li> <a href="#support"> FAQ </a> </li>
+              </ul>
+           </div>
+            */}
            
-           <div class="column links">
-             {/* <h3>S</h3> */}
-              <ul>
-               <li>
-                <a href="#faq">F.A.Q</a>
-               </li>
-               <li>
-                <a href="#cookies-policy">COOKIES POLICY</a>
-               </li>
-               <li>
-               <a href="#terms-of-services">TERMS OF SERVICE</a>
-               </li>
-               <li>
-               <a href="#support">SUPPORT</a>
-               </li>
+           <div className="column links">
+           <h4>Useful Links</h4>
+            <ul>
+               <li> <a href="#faq">F.A.Q</a> </li>
+               <li> <a href="#cookies-policy">COOKIES POLICY</a> </li>
+               <li> <a href="#terms-of-services">TERMS OF SERVICE</a> </li>
+               <li> <a href="#support">SUPPORT</a> </li>
+               <li> <a href="#support">CONTACT</a> </li>
              </ul>
            </div>
-           <div class="column links">
-             {/* <h3>S</h3> */}
-              <ul>
-               <li>
-                <a href="#faq">CAREERS</a>
-               </li>
-               <li>
-                <a href="#cookies-policy">ARCHIVE</a>
-               </li>
-               <li>
-               <a href="#terms-of-services">USER AGREEMENT</a>
-               </li>
-               <li>
-               <a href="#support">COMMENT POLICY</a>
-               </li>
 
-               <li>
-               <a href="#support">DMCA POLICY</a>
-               </li>
-             </ul>
+           <div className="column links">
+           <h4>Policies</h4>
+            <ul>
+              <li> <a href="#faq">CAREERS</a> </li>
+              <li> <a href="#cookies-policy">ARCHIVE</a> </li>
+              <li> <a href="#terms-of-services">USER AGREEMENT</a> </li>
+              <li> <a href="#support">COMMENT POLICY</a> </li>
+              <li> <a href="#support">DMCA POLICY</a> </li>
+            </ul>
            </div>
+        
+          </div>
            
-           {/* <div class="column subscribe">
-            <h3>Newsletter</h3>
-             <div>
-              <input type="email" placeholder="Your email id here" />
-              <button>Subscribe</button>
-             </div>
-           
-           </div> */}
-           
-           </div>
-           
-           <div class="row copyright">
-             {/* <div class="footer-menu">
-           
-             <a href="">Home</a>
-             <a href="">About</a>
-             <a href="">Contact</a>
-             <a href="">Blog</a>
-             <a href="">Social</a>
-           
-             </div> */}
-             <hr />
-              <p className="">Copyright &copy; 2022 Dev. ATM</p>
+          <hr />
+           <div className="copyright">
+            <p className="text-info"> &copy; Copyright Dev Team</p>
            </div>
            
                                
-               </div>
+      </div>
     </div>
+
+ </section>
+
+
+
+
+     
+
+
+
+
+
+        </div>
+    
    
-    </>
+   
   )
 }
 
